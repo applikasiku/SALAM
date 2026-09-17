b7d1914a8471fee8cd9476a3744a9777bc29fec8
applikasiku <amelnewsid@gmail.com>
2026-09-18 04:02:04 +0700

    feat: prepare SALAM for Android APK and AAB

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'id.salam.app',
+  appName: 'SALAM',
+  webDir: 'dist',
+  backgroundColor: '#F53B60'
+};
+export default config;
